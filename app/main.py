c9e5cdcdde0f254ff86873b1f6eb6c36ab5e6f54
Amruthaarunachalam <amruthaarunachalam@gmail.com>
2026-08-26 15:58:39 +0530

    corrected code

--- app/main.py
+++ app/main.py
@@ -1,14 +1,19 @@
-# main.py
+# app/main.py
 from fastapi import FastAPI
-import app.models
-from app.database import engine
-from app.routers import categories,products
-models.Base.metadata.create_all(bind=engine)
+from app.database import Base, engine
+
+from app.models import category as category_model
+from app.models import products as product_model
+
+from app.routers import categories as categories_router
+from app.routers import products as products_router
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="E-Commerce Backend")
 
-app.include_router(categories.router)
-app.include_router(products.router)
+app.include_router(categories_router.router)
+app.include_router(products_router.router)
 
 
 @app.get("/")
--- app/models/category.py
+++ app/models/category.py
@@ -1,5 +1,5 @@
 from app.database import Base
-from sqlalchemy import Coulmn,Integer,String
+from sqlalchemy import Column,Integer,String
 class CategoryModel(Base):
     __tablename__="category"
     id=Column(Integer,primary_key=True,index=True)
